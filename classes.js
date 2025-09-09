0ebe5f063c4999e0b2db88b37e6d121329f0e913
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-09-09 15:40:36 -0400

    (feat) add factory HashMap() & method hash(key).
    -> using factory functions.

--- classes.js
+++ classes.js
@@ -1,2 +1,18 @@
 #!/usr/bin/node
 
+function HashMap() {
+	let loadFactor;
+	let capacity;
+
+	const hash = function getHashCode(key) {
+		let hashCode = 0;
+		const primeNumber = 31;
+		for (let i = 0; i < key.length; i++) {
+			hashCode = primeNumber * hashCode + key.charCodeAt(i);
+			hashCode %= 16;
+		}
+		return hashCode;
+	};
+
+	return { hash, capacity, loadFactor };
+}
